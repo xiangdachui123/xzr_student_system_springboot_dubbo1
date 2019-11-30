3eec33ca7530df1b5050de67b8a3287e0f1c0df8
janhu <497955814@qq.com>
2019-11-30 14:11:58 +0800

    建了班级的实体类

---
+++ zxr_entity/src/main/java/com/qf/entity/Classes.java
@@ -0,0 +1,23 @@
+package com.qf.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * 班级的实体类
+ * @author zhan_jh
+ * @Date 2019/11/30
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+public class Classes extends BaseEntity {
+
+    private String cname;
+    private Integer cnum;
+    private Integer tid;
+}
