5978f48e055b3363de2cc678dd370e4165ba6338
DaChui <123@qq.com>
2019-11-30 11:36:41 +0800

    提交各maven项目

---
+++ xzr_commons/src/main/java/com/qf/tools/test.java
@@ -0,0 +1,8 @@
+package com.qf.tools;
+
+/**
+ * @auther xiangjun
+ * @Date 2019/11/30
+ */
+public class test {
+}
---
+++ zxr_entity/src/main/java/com/qf/entity/Stu.java
@@ -0,0 +1,8 @@
+package com.qf.entity;
+
+/**
+ * @auther xiangjun
+ * @Date 2019/11/30
+ */
+public class Stu {
+}
